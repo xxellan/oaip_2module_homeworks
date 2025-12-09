043b0d620b8eae7503336ef43860744ef42ec3e0
chetverikova.valeriya <chetverikova.valerka@gmail.com>
2025-12-09 20:07:39 +0400

    Add files via upload
    задачи на классы

---
+++ dz13.cpp
@@ -0,0 +1,70 @@
+//задачи на классы (2 последние)
+#include <iostream>
+#include <locale>
+#include <string>
+using namespace std;
+class Shape {
+public:
+    int a;
+};
+class Publication {
+private:
+    string title;
+public:
+    Publication() { title = " "; };
+    Publication(string t) :title(t) {};
+    void displayInfo() {
+        cout << "Name of publication "<<title << endl;
+    }
+    void Gettitle(string t) {
+        title = t;
+    }
+};
+class Book: public Publication {
+private:
+    string name;
+    string author;
+    int year;
+public:
+    Book(string name, string author, int year) {
+        this->name = name;
+        this->author = author;
+        this->year = year;
+    }
+    void displayinfo() {
+        cout << "Name: " + name << endl;
+        cout << "Author: " + author << endl;
+        cout << "Year of publication: " << year << endl;
+    }
+};
+class Rectangle: public Shape {
+private:
+    int b;
+public:
+    Rectangle(int a,int b) {
+        this->b = b;
+        this->a = a;
+    }
+    int calculateArea() {
+        return a * b;
+    }
+    int calculatePerimeter() {
+        return (a + b) * 2;
+    }
+    void displayinfo() {
+        cout << "Length: " << a << ", " << "Width: " << b << ", " << "Площадь: " << a*b << ", " << "Периметр: " << (a + b) *2<< endl;
+    }
+};
+
+int main()
+{
+    Book k1("Crime and Punishment", "Dostoevsky", 1865);
+    k1.Gettitle("Классика");
+    k1.displayInfo();
+    k1.displayinfo();
+    Rectangle f1(23, 5);
+    int s = f1.calculateArea();
+    int p = f1.calculatePerimeter();
+    f1.displayinfo();
+
+}
